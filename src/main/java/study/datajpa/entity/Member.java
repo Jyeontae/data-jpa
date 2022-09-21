0a2e9a4f932b24165a99685e39b6192e57fd0c4d
Yeontae Jeong <jyeontae>
2022-09-21 16:37:54 +0900

    Initial commit 스프링 데이터 실습

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,27 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+    protected Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public void changeUserName(String username){
+        this.username = username;
+    }
+}
